9527b34209721d0b748ed580e80566f0d0c91b9f
Oleksii Nadtochyi <a.nadtochey@gmail.com>
2022-04-26 12:21:23 +0300

    Recursion functions were added.

--- index.js
+++ index.js
@@ -234,3 +234,30 @@ const twiceOpt2 = function (number) {
 }
 /* lost of flexibility */
 console.log(twice(5))
+
+// ********** recursion **********
+function powerRec(base, exponent) {
+    if (exponent == 0)  return 1
+    else                return base * power(base, exponent - 1)
+
+    // works slower that loop version because of creation of environment
+}
+
+console.log(powerRec(2, 3))
+
+function findSolution(target) {
+    function find(current, history) {
+        if (current == target) {
+            return history
+        } else if (current > target) {
+            return null
+        } else {
+            return find(current + 5, `(${history} + 5)`) ||
+                find(current * 3, `(${history} * 3)`)
+        }
+    }
+
+    return find(1, "1")
+}
+
+console.log(findSolution(24))
