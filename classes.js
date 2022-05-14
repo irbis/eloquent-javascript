55c290b825457518a4508f107493c05961ed065b
Oleksii Nadtochyi <a.nadtochey@gmail.com>
2022-05-14 10:15:53 +0300

    The Secret Life of Objects: The Interator Interface.

--- classes.js
+++ classes.js
@@ -182,3 +182,73 @@ let stringObject = {
     [toStringSymbol]() { return "a jute rope" }
 }
 console.log(stringObject[toStringSymbol]())
+
+// The iterator interface
+let okIterator = "OK"[Symbol.iterator]()
+console.log(okIterator.next()) // expecting value: O, done: false
+console.log(okIterator.next()) // expecting value: K, done: false
+console.log(okIterator.next()) // expecting value: undefined, done: true
+
+class MatrixIterator {
+    constructor(matrix) {
+        this.x      = 0
+        this.y      = 0
+        this.matrix = matrix
+    }
+
+    /*
+      return { x, y, value }
+     */
+    next() {
+        if (this.y == this.matrix.height) return { done: true }
+
+        let value = { x:     this.x,
+                      y:     this.y,
+                      value: this.matrix.get(this.x, this.y) }
+        this.x++
+        if (this.x == this.matrix.width) {
+            this.x = 0
+            this.y++
+        }
+
+        return { value, done: false }
+    }
+
+}
+
+class Matrix {
+    constructor(width, height, element = (x, y) => undefined) {
+        this.width = width
+        this.height = height
+        this.content = []
+
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                this.content[y * width + x] = element(x, y)
+            }
+        }
+    }
+
+    get(x, y) {
+        return this.content[y * this.width + x]
+    }
+    set (x, y, value) {
+        this.content[y * this.width + x] = value
+    }
+
+    [Symbol.iterator]() {
+        return new MatrixIterator(this)
+    }
+}
+
+/*
+   Another method to setup iterator:
+   Matrix.prototype[Symbol.iterator] = function() {
+       return new MatrixIterator(this)
+   }
+ */
+
+let matrix = new Matrix(2, 2, (x, y) => `value ${x},${y}`)
+for (let { x, y, value } of matrix) {
+    console.log(x, y, value)
+}
