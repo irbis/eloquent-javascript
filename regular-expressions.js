e8a7f721187e146f05c0d7499d8d38ea9eaccc7d
Alexsey Nadtochey <a.nadtochey@gmail.com>
2022-05-27 15:23:57 +0300

    Regular Expressions. Exercises, regexp golf.

--- regular-expressions.js
+++ regular-expressions.js
@@ -148,3 +148,53 @@ let matchLoop
 // the below is possible because lastIndex updates accordinaly
 while (matchLoop = number.exec(input))
   console.log(`Found ${matchLoop[0]}, at ${matchLoop.index}`)
+
+//Exercises
+/*
+  1. car and cat
+  2. pop and prop
+  3. ferret, ferry, and ferrari
+  4. Any word ending in ious
+  5. A whitespace character followed by a period, comma, colon, or semicolon
+  6. A word longer than six letters
+  7. A word without the letter e (or E)
+ */
+
+function verify(regexp, yes, no) {
+    // Ignore unfinished exercises
+    if (regexp.source == "...") return
+    for (let str of yes) if (!regexp.test(str)) {
+        console.log(`Failure to match '${str}'`)
+    }
+    for (let str of no) if (regexp.test(str)) {
+        console.log(`Unexpected match for '${str}'`)
+    }
+}
+
+verify(/ca[rt]/,
+    ["my car", "bad cats"],
+    ["camper", "high art"])
+
+verify(/pr?op/,
+    ["pop culture", "mad props"],
+    ["plop", "prrrop"]);
+
+verify(/ferr(et|y|ari)/,
+    ["ferret", "ferry", "ferrari"],
+    ["ferrum", "transfer A"]);
+
+verify(/ious\b/,
+    ["how delicious", "spacious room"],
+    ["ruinous", "consciousness"]);
+
+verify(/\s[.,:;]/,
+    ["bad punctuation ."],
+    ["escape the period"]);
+
+verify(/\w{7}/,
+    ["Siebentausenddreihundertzweiundzwanzig"],
+    ["no", "three small words"]);
+
+verify(/\b[^\We]\b/i,
+    ["red platypus", "wobbling nest"],
+    ["earth bed", "learning ape", "BEET"]);
