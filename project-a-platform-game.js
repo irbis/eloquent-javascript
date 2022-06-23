8494fe734711a185a90773045f32dcae7c6d093f
Alexsey Nadtochey <a.nadtochey@gmail.com>
2022-06-23 18:09:22 +0300

    Project: A Platform Game, part #2: viewport.

--- project-a-platform-game.js
+++ project-a-platform-game.js
@@ -121,5 +121,78 @@ class Level {
     }
 }
 
-let simpleLevel = new Level(simpleLevelPlan)
-console.log(`${simpleLevel.width} by ${simpleLevel.height}`) // -> 22 by 9
+function elt(name, attrs, ...childer) {
+    let dom = document.createElement(name)
+
+    Object.keys(attrs).forEach( attr => dom.setAttribute(attr, attrs[attr]) )
+    childer.forEach( child => dom.appendChild(child) )
+
+    return dom
+}
+
+class DOMDisplay {
+    constructor(parent, level) {
+        this.dom = elt("div", {class: "game"}, drawGrid(level))
+        this.actorLayer = null
+        parent.appendChild(this.dom)
+    }
+
+    clear() { this.dom.remove() }
+}
+
+DOMDisplay.prototype.syncState = function (state) {
+    if (this.actorLayer) this.actorLayer.remove()
+    this.actorLayer = drawActors(state.actors)
+    this.dom.appendChild(this.actorLayer)
+    this.dom.className = `game ${state.status}`
+    this.scrollPlayerIntoView(state)
+}
+
+DOMDisplay.prototype.scrollPlayerIntoView= function (state) {
+    let width = this.dom.clientWidth
+    let height = this.dom.clientHeight
+    let margin = width / 3
+
+    // The viewport
+    let left = this.dom.scrollLeft
+    let right = left + width
+    let top = this.dom.scrollTop
+    let bottom = top + height
+
+    let player = state.player
+    let center = player.pos.plus(player.size.times(0.5)).times(scale)
+
+    if (center.x < left + margin) {
+        this.dom.scrollLeft = center.x - margin
+    } else if (center.x > right - margin) {
+        this.dom.scrollLeft = center.x + margin - width
+    }
+    if (center.y < top + margin) {
+        this.dom.scrollTop = center.y - margin
+    } else if (center.y > bottom - margin) {
+        this.dom.scrollTop = center.y + margin - height
+    }
+}
+
+const scale = 20
+
+function drawGrid(level) {
+    return elt("table", {
+        class:  "background",
+        style: `width: ${level.width * scale}px`},
+        ...level.rows.map(row =>
+            elt("tr", {style: `height: ${scale}px`},
+                ...row.map(type => elt("td", {class: type})))
+        ))
+}
+
+function drawActors(actors) {
+    return elt("div", {}, ...actors.map(actor => {
+        let rect = elt("div", {class: `actor ${actor.type}`})
+        rect.style.width  = `${actor.size.x * scale}px`
+        rect.style.height = `${actor.size.y * scale}px`
+        rect.style.left   = `${actor.pos.x * scale}px`
+        rect.style.top    = `${actor.pos.y * scale}px`
+        return rect
+    }))
+}
